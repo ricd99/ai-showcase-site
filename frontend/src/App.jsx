a718e7186b97cc3fe683340f8b21ef1fc867545e
Ryan Huang <ryanhuang6391@gmail.com>
2025-11-26 23:01:07 -0800

    website skeleton

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,29 @@
 import React from "react";
 import "./App.css";
-import ProjectList from "./components/Projects.jsx"
+import ProjectList from "./components/Projects.jsx";
 
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Project Showcase</h1>
-      </header>
+      <div>
+        <header className="App-header">
+          <h1>My AI Projects</h1>
+        </header>
+        <p>
+          These projects follow the structure of Andrej Karpathy's Zero To Hero
+          series.
+        </p>
+        <p>
+          Each cell below is one lecture from the series. Within each cell is
+          the project from that lecture, as well as side projeects I've done
+          related to that lecture's topic.
+        </p>
+      </div>
       <main>
         <ProjectList />
       </main>
     </div>
-  )
-}
+  );
+};
 
 export default App;
